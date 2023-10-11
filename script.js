25fbb5f8edec68e394011aacaaff0d5b4400d1a5
yusuferens <yusuferenketen@gmail.com>
2023-10-11 21:19:24 +0300

    made a basic interface  for the game and script

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+document.getElementById("rock").addEventListener("click", function () {
+    playRound("rock");
+});
+
+document.getElementById("paper").addEventListener("click", function () {
+    playRound("paper");
+});
+
+document.getElementById("scissors").addEventListener("click", function () {
+    playRound("scissors");
+});
+
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
 
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        document.getElementById("result").innerText = "It's a tie!";
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        document.getElementById("result").innerText = `You win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        document.getElementById("result").innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
@@ -20,8 +31,36 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
 }
+document.getElementById("rock").addEventListener("click", function () {
+    playRound("rock");
+});
 
-const userInput = prompt("Please enter your choice: rock, paper, or scissors");
-const computerSelection = getComputerChoice();
+document.getElementById("paper").addEventListener("click", function () {
+    playRound("paper");
+});
 
-console.log(playRound(userInput, computerSelection));
+document.getElementById("scissors").addEventListener("click", function () {
+    playRound("scissors");
+});
+
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        document.getElementById("result").innerText = "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        document.getElementById("result").innerText = `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        document.getElementById("result").innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
