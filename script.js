3e7a621393014ef9f8bee35023edfa5422fe9948
yusuferens <yusuferenketen@gmail.com>
2023-10-11 19:28:26 +0300

    finished the script for the game

---
+++ script.js
@@ -0,0 +1,27 @@
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
+
+const userInput = prompt("Please enter your choice: rock, paper, or scissors");
+const computerSelection = getComputerChoice();
+
+console.log(playRound(userInput, computerSelection));
