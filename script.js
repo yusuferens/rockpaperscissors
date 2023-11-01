c18564ccf3cfda2df960ca8c44cd9bed252394cd
yusuferens <yusuferenketen@gmail.com>
2023-11-01 15:09:26 +0300

    made functions to randomize computer choice

--- script.js
+++ script.js
@@ -4,4 +4,36 @@ const scissorButton = document.querySelector('[data-scissors]')
 const paperButton = document.querySelector('[data-paper]')
 const playAgainButton = document.querySelector('[data-replay]')
 const roundCounter = document.querySelector('[data-round-counter]')
+const roundCounterComp = document.querySelector('[data-round-counter-comp]')
 const roundResult = document.querySelector('[data-result]')
+
+
+function rpsGame(){
+    this.scorePlayer = 0;
+    this.scoreComputer = 0;
+    this.currentRound = 1;
+}
+
+function getComputerchoice(){
+    const choices = ['rock','paper','scissors']
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex]
+}
+
+function determineWinner(userChoice, computerChoice){
+    if (userChoice === computerChoice) {
+        return 'It is a Tie!'
+    }
+    if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'paper' && computerChoice === 'rock') ||
+        (userChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        return 'You win!';
+    } else {
+        return 'You lose!';
+    }
+
+}
+
+
