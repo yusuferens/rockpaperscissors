0898b4ee0e2836b5c28dab9609fe923c0b716c08
yusuferens <yusuferenketen@gmail.com>
2023-11-01 14:47:18 +0300

    cleared previous scripts and added consts

--- script.js
+++ script.js
@@ -1,66 +1,7 @@
-document.getElementById("rock").addEventListener("click", function () {
-    playRound("rock");
-});
 
-document.getElementById("paper").addEventListener("click", function () {
-    playRound("paper");
-});
-
-document.getElementById("scissors").addEventListener("click", function () {
-    playRound("scissors");
-});
-
-function playRound(playerSelection) {
-    const computerSelection = getComputerChoice();
-
-    if (playerSelection === computerSelection) {
-        document.getElementById("result").innerText = "It's a tie!";
-    } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-        document.getElementById("result").innerText = `You win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-        document.getElementById("result").innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
-    }
-}
-
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * 3);
-    return choices[randomIndex];
-}
-document.getElementById("rock").addEventListener("click", function () {
-    playRound("rock");
-});
-
-document.getElementById("paper").addEventListener("click", function () {
-    playRound("paper");
-});
-
-document.getElementById("scissors").addEventListener("click", function () {
-    playRound("scissors");
-});
-
-function playRound(playerSelection) {
-    const computerSelection = getComputerChoice();
-
-    if (playerSelection === computerSelection) {
-        document.getElementById("result").innerText = "It's a tie!";
-    } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-        document.getElementById("result").innerText = `You win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-        document.getElementById("result").innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
-    }
-}
-
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * 3);
-    return choices[randomIndex];
-}
+const rockButton = document.querySelector('[data-rock]')
+const scissorButton = document.querySelector('[data-scissors]')
+const paperButton = document.querySelector('[data-paper]')
+const playAgainButton = document.querySelector('[data-replay]')
+const roundCounter = document.querySelector('[data-round-counter]')
+const roundResult = document.querySelector('[data-result]')
